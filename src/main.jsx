82e66ef4524aa76bbad9fc68f0343bc9b4384e79
whis102 <chicongnguyen.dev@gmail.com>
2024-05-02 20:32:41 +0700

    update ReactRouterDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import GlobalStyles from "./components/GlobalStyles/GlobalStyles.jsx";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import GlobalStyles from "./components/GlobalStyles/GlobalStyles.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </GlobalStyles>
   </React.StrictMode>
-);
+)
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,61 @@
+import React from "react"
+import "./Register.scss"
+import Checkbox from "@mui/material/Checkbox"
+import TextInput from "../../components/TextInput/TextInput"
+import LoginBtn from "../../components/LoginBtn/LoginBtn"
+
+function Register() {
+    return (
+        <div className="container">
+            <div className="login">
+                <div className="login__brand">
+                    <img src="./favicon.png" width={"45px"} />
+                    <h1>SkyEdge</h1>
+                </div>
+
+                <div className="login__content">
+
+                    <div className="login__title">
+                        <h1>Welcome back!</h1>
+                        <p>Please enter your details</p>
+                    </div>
+
+                    <form action="">
+                        <TextInput
+                            id={"outlined-password-input"}
+                            label={"Email"}
+                            type={"text"}
+                        />
+
+                        <TextInput
+                            id={"outlined-password-input"}
+                            label={"Password"}
+                            type={"password"}
+                            autoComplete={"current-password"}
+                        />
+
+                        <div className="options">
+                            <div>
+                                <Checkbox defaultUnChecked size="small" />
+                                <label>Keep me logged in</label>
+                            </div>
+
+                            <a href="/forgot-password" className="forgot-password">Forgot password</a>
+                        </div>
+
+                        <LoginBtn children={"Login"} />
+                        <div className="signup-option">
+                            Don't have an account? <a href="/signup">Sign up</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div className="login__image">
+                <img src="src/assets/background.png" alt="Login Background" />
+            </div>
+        </div>
+    )
+}
+
+export default Register
